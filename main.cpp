abc74d21f0e7fc53f9d0ea0ae2ba4dc59624ab99
Michelle Chavez <al554699@edu.uaa.mx>
2025-06-23 17:14:58 -0600

    Agregamos las funciones para gener una carta aleatoria con su valor, agregar una carta a la mano del jugador y para mostrar la mano actual de un jugador

--- main.cpp
+++ main.cpp
@@ -29,3 +29,49 @@ struct Partida{
     string resultado;
     string fecha;
 };
+
+// Funcion que genera una carta aleatoria con su valor
+Carta generarCarta(){
+    string nombres[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int valores[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
+    int i = rand()% 13; // Número aleatorio del 0 al 12
+    return{nombres[i], valores[i]};
+}
+
+
+// Funcion para agregar una carta a la mano del jugador
+void agregarCarta(Jugador &jugador){
+    Carta nueva = generarCarta();
+    Carta* nuevaMano = new Carta[jugador.numCartas + 1];  // Crear un nuevo arreglo con una carta adicional
+
+    for(int i = 0; i < jugador.numCartas; i++) // Copiar las cartas existentes
+        nuevaMano[i] = jugador.mano[i];
+
+    nuevaMano[jugador.numCartas] = nueva;
+    delete[] jugador.mano;
+    jugador.mano = nuevaMano; // Actualizar la mano
+    jugador.numCartas++;
+
+    jugador.puntos += nueva.valor; // Sumar el valor de la carta
+
+    // Ajustar si hay un As y el jugador se pasa de 21
+    if(jugador.puntos > 21){
+        for (int i = 0; i < jugador.numCartas; i++){
+            if(jugador.mano[i].nombre == "A" && jugador.mano[i].valor == 11){
+                jugador.mano[i].valor = 1;
+                jugador.puntos -= 10;
+                break;
+            }
+        }
+    }
+}
+
+
+// Función para mostrar la mano actual de un jugador
+void mostrarMano(const Jugador &jugador){
+    cout << jugador.nombre << " tiene: ";
+    for (int i = 0; i < jugador.numCartas; i++)
+        cout << jugador.mano[i].nombre << " ";
+    cout << "  Puntos: " << jugador.puntos << endl;
+}
